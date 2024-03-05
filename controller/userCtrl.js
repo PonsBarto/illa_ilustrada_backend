62d3dd9e62d2253b6284de74488b402ff38767d1
Barto <ponsbarto@gmail.com>
2024-03-05 15:15:00 +0100

    traer un usuario a traves del login

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,16 +1,27 @@
 const User = require("../models/userModel");
-const asyncHandler=require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
-  const findUser = await User.findOne({ email:email });
+  const findUser = await User.findOne({ email: email });
   if (!findUser) {
     //create new User
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
     //User Already Exists
-    throw new Error ("User Already Exists")
+    throw new Error("User Already Exists");
   }
 });
 
-module.exports = { createUser };
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //check if user exists or not
+  const findUser = await User.findOne({ email });
+  if (findUser && await findUser.isPasswordMatched(password)) {
+    res.json(findUser);
+  } else {
+    throw new Error("Invalid Email Or Password");
+  }
+});
+
+module.exports = { createUser, loginUserCtrl };
