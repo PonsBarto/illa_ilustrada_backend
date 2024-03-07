6e9e01872af93b97d8c6bdebd45e68324cfb7a3f
Barto <ponsbarto@gmail.com>
2024-03-07 09:01:42 +0100

    verificacioon del token e implementar el orden del listado de usuraios me falla, parace que hay errores de importacion

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,14 +7,13 @@ const {
   deleteaUser,
   updatedUser,
 } = require("../controller/userCtrl");
+const {authMiddleware, isAdmin}= require("../middlewares/authMiddleware");
 const router = express.Router();
-
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
 router.put("/:id", updatedUser);
-
 
 module.exports = router;
