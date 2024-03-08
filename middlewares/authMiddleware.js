f7c555c66289fa255d285ceca41d4079dee4d8fa
Barto <ponsbarto@gmail.com>
2024-03-08 11:26:49 +0100

    correccion para la obtencion del token era necesario un espacio en la fila diez de authMiddleware.js en el parentesis despues de split

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const asyncHandler = require("express-async-handler");
 const authMiddleware = asyncHandler(async (req, res, next) => {
   let token;
   if (req?.headers?.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split("")[1];
+    token = req.headers.authorization.split(" ")[1];
     try {
         if(token){
             const decoded= jwt.verify(token,process.env.JWT_SECRET);
