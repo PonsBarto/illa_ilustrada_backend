8d0364a9dd99e8c40c1d6ae0510855d197a45dd1
Barto <ponsbarto@gmail.com>
2024-03-12 09:03:40 +0100

    implementado metodo de logout

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbId");
 const { generateRefreshToken } = require("../config/refreshtoken");
+const jwt = require("jsonwebtoken");
 //Create a User
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -46,6 +47,47 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   } else {
     throw new Error("Invalid Email Or Password");
   }
+});
+
+//handle refresh token
+
+const handelRefreshToken = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie.refreshToken) throw new Error("No Refresh Token in Cookies");
+  const refreshToken = cookie.refreshToken;
+  const user = await User.findOne({ refreshToken });
+  if (!user) throw new Error("No refresh token in db or matched");
+  jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err || user.id !== decoded.id) {
+      throw new Error("Ther is something wrong with refresh token");
+    }
+    const accessToken = generateToken(user?._id);
+    res.json({ accessToken });
+  });
+});
+
+//Logout funcionality
+
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie.refreshToken) throw new Error("No Refresh Token in Cookies");
+  const refreshToken = cookie.refreshToken;
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+    });
+    return res.sendStatus(204); //forbidden
+  }
+  await User.findOneAndUpdate(refreshToken, {
+    refreshToken: "",
+  });
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+  });
+  res.sendStatus(204); //forbidden
 });
 
 //Update a user
@@ -165,4 +207,6 @@ module.exports = {
   updatedUser,
   blockUser,
   unblockUser,
+  handelRefreshToken,
+  logout,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,16 +8,21 @@ const {
   updatedUser,
   blockUser,
   unblockUser,
+  handelRefreshToken,
+  logout,
 } = require("../controller/userCtrl");
 const {authMiddleware, isAdmin}= require("../middlewares/authMiddleware");
 const router = express.Router();
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
+router.get("/refresh", handelRefreshToken,);
+router.get("/logout", logout);
 router.get("/:id", authMiddleware, isAdmin, getaUser);
 router.delete("/:id", deleteaUser);
 router.put("/edit-user", authMiddleware, updatedUser,);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser,);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser,);
+
 
 module.exports = router;
