a3f90c9986c76351c28be87270735fec3da62ce6
Barto <ponsbarto@gmail.com>
2024-03-12 14:13:21 +0100

    traer productos por idy crear productos implementado con existo

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,10 +1,22 @@
-const Product =require("../models/productModel");
-const asyncHandler = require ("express-async-handler");
+const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
 
-
-const createProduct =  asyncHandler(async (req, res)=>{
-    res.json({
-        message: "Hey it's product post route"
-    });
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
-module.exports={ createProduct };
+
+const getaProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProduct = await Product.findById(id);
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = { createProduct, getaProduct, };
