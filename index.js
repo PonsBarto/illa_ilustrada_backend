6c24a7b231e8a21e21985d7bf40f9e1ce0eb4787
Barto <ponsbarto@gmail.com>
2024-03-15 16:59:14 +0100

    realizada la funcionalidad completa de prodcategory

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const  productRouter = require ("./routes/productRoute");
 const  blogRouter = require ("./routes/blogRoute");
+const categoryRouter = require ("./routes/prodctcategoryRoute")
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const blogModel = require("./models/blogModel");
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
 
 
 
