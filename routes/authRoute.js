bd34342413b975556058b46f52644952c5c0185a
Barto <ponsbarto@gmail.com>
2024-03-20 16:52:39 +0100

    createOrder crea una orden de compra, extrae los datos solicitados verifica el id de dusuraio, verifica metodo de pago, recibe el carrito del usuarios, calcula cantidad final, actualiza la cantidad de stock y ventas realizadas e inforam al cliente de que la compora se ha realizado

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -20,6 +20,7 @@ const {
   getUserCart,
   emptyCart,
   applyCoupon,
+  createOrder,
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -31,6 +32,7 @@ router.post("/login", loginUserCtrl);
 router.post("/admin-login", loginAdmin);
 router.post("/cart", userCart);
 router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.post("/cart/cash-order", authMiddleware, createOrder);
 router.get("/all-users", getallUser);
 router.get("/refresh", handelRefreshToken);
 router.get("/logout", logout);
